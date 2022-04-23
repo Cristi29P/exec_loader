bf7e26ac34f894af008d21f6958bc62e5b107313
Cristian Paris <cristian.tanasep@gmail.com>
2022-04-23 10:54:31 +0300

    Implemented so_init_loader function

--- skel-lin/loader/loader.c
+++ skel-lin/loader/loader.c
@@ -7,15 +7,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #include "exec_parser.h"
 
 static so_exec_t *exec;
 
+
+
+void fault_handler() {
+
+}
+
+
 int so_init_loader(void)
 {
-	/* TODO: initialize on-demand loader */
-	return -1;
+	struct sigaction signal_action;
+
+	memset(&signal_action, 0, sizeof(struct sigaction));
+
+	signal_action.sa_flags = SA_SIGINFO;
+	signal_action.sa_sigaction = fault_handler;
+
+	sigemptyset(&signal_action.sa_mask);
+	sigaddset(&signal_action.sa_mask, SIGSEGV);
+
+	if (sigaction(SIGSEGV, &signal_action, NULL)) {
+		fprintf(stderr, "Failed initialization\n");
+		exit(-1);
+	}
+
+	/* Initialization ended successfully */
+	return 0;
 }
 
 int so_execute(char *path, char *argv[])
